15955da81c00c8b5ee8ad4c24722dae6c1d34225
Makar Barzikov <makarkosiv@gmail.com>
2018-06-20 11:05:09 +0300

    final version:

---
+++ app/src/main/java/com/mbarz/samplegame/Heliboy.java
@@ -0,0 +1,8 @@
+package com.mbarz.samplegame;
+public class Heliboy extends Enemy {
+
+    public Heliboy(int centerX, int centerY) {
+        setCenterX(centerX);
+        setCenterY(centerY);
+    }
+}
---
+++ app/src/main/java/com/mbarz/samplegame/SplashLoadingScreen.java
@@ -0,0 +1,47 @@
+package com.mbarz.samplegame;
+
+import com.mbarz.framework.Game;
+import com.mbarz.framework.Graphics;
+import com.mbarz.framework.Graphics.ImageFormat;
+import com.mbarz.framework.Screen;
+
+public class SplashLoadingScreen extends Screen {
+    public SplashLoadingScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        Graphics g = game.getGraphics();
+        Assets.splash= g.newImage("splash.jpg", ImageFormat.RGB565);
+
+
+        game.setScreen(new LoadingScreen(game));
+
+    }
+
+    @Override
+    public void paint(float deltaTime) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void backButton() {
+
+    }
+}
